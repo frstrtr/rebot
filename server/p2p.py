5890c005a0281cf63339d6964ee1f4bd70711165
frstrtr <frstrtr@gmail.com>
2025-02-04 19:10:48 +0400

    handshake implementation

--- server/p2p.py
+++ server/p2p.py
@@ -18,6 +18,8 @@ PURPLE_COLOR = "\033[95m"
 INVERSE_COLOR = "\033[7m"
 RESET_COLOR = "\033[0m"
 
+HANDSHAKE_INIT = "handshake_init"
+HANDSHAKE_RESPONSE = "handshake_response"
 
 class PeerAddress(IPv4Address):
     """Custom class that extends IPv4Address and includes a UUID property"""
@@ -32,7 +34,7 @@ class P2PProtocol(protocol.Protocol):
 
     def connectionMade(self):
         """Handle new P2P connections."""
-        peer = self.getPeer()
+        peer = self.get_peer()
         self.factory.peers.append(peer)
         LOGGER.info(
             "%sP2P connection made with %s:%d%s",
@@ -42,77 +44,117 @@ class P2PProtocol(protocol.Protocol):
             RESET_COLOR,
         )
         LOGGER.info("%sP2P connection details: %s%s", YELLOW_COLOR, peer, RESET_COLOR)
-        self.send_peer_info()
-        # Send local UUID to the remote node
-        self.transport.write(
-            json.dumps({"uuid": self.factory.node_uuid}).encode("utf-8")
-        )
+        self.send_handshake_init()
 
-    def getPeer(self):
+    def get_peer(self):
         """Override getPeer to return PeerAddress with UUID."""
         peer = self.transport.getPeer()
         return PeerAddress(peer.type, peer.host, peer.port, self.factory.node_uuid)
 
+    def send_handshake_init(self):
+        """Send handshake initiation message."""
+        message = json.dumps({"type": HANDSHAKE_INIT, "uuid": self.factory.node_uuid})
+        self.transport.write(message.encode("utf-8"))
+        LOGGER.info("Sent handshake initiation: %s", message)
+
+    def send_handshake_response(self, node_uuid):
+        """Send handshake response message."""
+        message = json.dumps({"type": HANDSHAKE_RESPONSE, "uuid": node_uuid})
+        self.transport.write(message.encode("utf-8"))
+        LOGGER.info("Sent handshake response: %s", message)
+
     def dataReceived(self, data):
         """Handle received P2P data."""
         message = data.decode("utf-8")
-        peer = self.getPeer()
+        peer = self.get_peer()
         LOGGER.debug(
             "P2P message received from %s:%d: %s", peer.host, peer.port, message
         )
 
         try:
-            # Split the message by '}{' and add the braces back
-            json_strings = self.split_json_objects(message)
-
-            for json_string in json_strings:
-                try:
-                    data = json.loads(json_string)
-                    data = self.decode_nested_json(data)
-                    # Log the decoded message in a human-readable format
-                    LOGGER.debug("Decoded message: %s", data)
-                    if "uuid" in data:
-                        # Update the peer's UUID with the received UUID
-                        peer.node_uuid = data["uuid"]
-                        LOGGER.info(
-                            "%sReceived UUID %s from peer %s:%d%s",
-                            GREEN_COLOR,
-                            peer.node_uuid,
-                            peer.host,
-                            peer.port,
-                            RESET_COLOR,
-                        )
-                    if "user_id" in data:
-                        user_id = data["user_id"]
-                        lols_bot_data = data.get("lols_bot_data", {})
-                        if isinstance(lols_bot_data, str):
-                            lols_bot_data = json.loads(lols_bot_data)
-                        cas_chat_data = data.get("cas_chat_data", {})
-                        if isinstance(cas_chat_data, str):
-                            cas_chat_data = json.loads(cas_chat_data)
-                        p2p_data = data.get("p2p_data", {})
-                        if isinstance(p2p_data, str):
-                            p2p_data = json.loads(p2p_data)
-                        if (
-                            p2p_data
-                            and isinstance(p2p_data, dict)
-                            and len(p2p_data) > 0
-                        ):
-                            store_spammer_data(
-                                user_id, lols_bot_data, cas_chat_data, p2p_data
-                            )
-                            self.factory.broadcast_spammer_info(user_id)
-                        else:
-                            # Store the spammer data even if p2p_data is empty
-                            store_spammer_data(
-                                user_id, lols_bot_data, cas_chat_data, p2p_data
-                            )
-                    elif "peers" in data:
-                        self.factory.update_peer_list(data["peers"])
-                except json.JSONDecodeError as e:
-                    LOGGER.error("Failed to decode JSON object: %s", e)
+            data = json.loads(message)
+            if data["type"] == HANDSHAKE_INIT:
+                self.handle_handshake_init(data)
+            elif data["type"] == HANDSHAKE_RESPONSE:
+                self.handle_handshake_response(data)
+            else:
+                self.handle_p2p_data(data)
         except json.JSONDecodeError as e:
             LOGGER.error("Failed to decode JSON: %s", e)
+
+    def handle_handshake_init(self, data):
+        """Handle handshake initiation message."""
+        peer_uuid = data["uuid"]
+        peer = self.get_peer()
+        peer.node_uuid = peer_uuid
+        LOGGER.info(
+            "%sReceived handshake initiation from %s:%d (UUID: %s)%s",
+            GREEN_COLOR,
+            peer.host,
+            peer.port,
+            peer.node_uuid,
+            RESET_COLOR,
+        )
+        if peer.node_uuid == self.factory.node_uuid:
+            LOGGER.info(
+                "%sDisconnecting self-connection to %s:%d (UUID: %s)%s",
+                RED_COLOR,
+                peer.host,
+                peer.port,
+                peer.node_uuid,
+                RESET_COLOR,
+            )
+            self.transport.loseConnection()
+        else:
+            self.send_handshake_response(self.factory.node_uuid)
+
+    def handle_handshake_response(self, data):
+        """Handle handshake response message."""
+        peer_uuid = data["uuid"]
+        peer = self.get_peer()
+        peer.node_uuid = peer_uuid
+        LOGGER.info(
+            "%sReceived handshake response from %s:%d (UUID: %s)%s",
+            GREEN_COLOR,
+            peer.host,
+            peer.port,
+            peer.node_uuid,
+            RESET_COLOR,
+        )
+        if peer.node_uuid == self.factory.node_uuid:
+            LOGGER.info(
+                "%sDisconnecting self-connection to %s:%d (UUID: %s)%s",
+                RED_COLOR,
+                peer.host,
+                peer.port,
+                peer.node_uuid,
+                RESET_COLOR,
+            )
+            self.transport.loseConnection()
+
+    def handle_p2p_data(self, data):
+        """Handle received P2P data."""
+        peer = self.get_peer()
+        LOGGER.debug("Handling P2P data from %s:%d", peer.host, peer.port)
+        if "user_id" in data:
+            user_id = data["user_id"]
+            lols_bot_data = data.get("lols_bot_data", {})
+            if isinstance(lols_bot_data, str):
+                lols_bot_data = json.loads(lols_bot_data)
+            cas_chat_data = data.get("cas_chat_data", {})
+            if isinstance(cas_chat_data, str):
+                cas_chat_data = json.loads(cas_chat_data)
+            p2p_data = data.get("p2p_data", {})
+            if isinstance(p2p_data, str):
+                p2p_data = json.loads(p2p_data)
+            if p2p_data and isinstance(p2p_data, dict) and len(p2p_data) > 0:
+                store_spammer_data(user_id, lols_bot_data, cas_chat_data, p2p_data)
+                self.factory.broadcast_spammer_info(user_id)
+            else:
+                # Store the spammer data even if p2p_data is empty
+                store_spammer_data(user_id, lols_bot_data, cas_chat_data, p2p_data)
+        elif "peers" in data:
+            self.factory.update_peer_list(data["peers"])
 
     def split_json_objects(self, message):
         """Split concatenated JSON objects in the message."""
@@ -158,7 +200,7 @@ class P2PProtocol(protocol.Protocol):
 
     def connectionLost(self, reason=protocol.connectionDone):
         """Handle lost P2P connections."""
-        peer = self.getPeer()
+        peer = self.get_peer()
         self.factory.peers = [
             p for p in self.factory.peers if p.host != peer.host or p.port != peer.port
         ]
@@ -189,25 +231,6 @@ class P2PFactory(protocol.Factory):
         self.node_uuid = node_uuid or str(uuid.uuid4())
         self.bootstrap_peers = []
 
-    def broadcast_spammer_info(self, user_id):
-        """Broadcast spammer information to all connected peers."""
-        spammer_data = retrieve_spammer_data_from_db(user_id)
-        if spammer_data:
-            # Ensure nested JSON data is properly encoded
-            message = json.dumps(
-                {
-                    "user_id": user_id,
-                    "lols_bot_data": spammer_data["lols_bot_data"],
-                    "cas_chat_data": spammer_data["cas_chat_data"],
-                    "p2p_data": spammer_data["p2p_data"],
-                }
-            )
-            for peer in self.peers:
-                peer.transport.write(message.encode("utf-8"))
-            LOGGER.info("Broadcasted spammer info: %s", message)
-        else:
-            LOGGER.warning("No spammer data found for user_id: %s", user_id)
-
     def connect_to_bootstrap_peers(self, bootstrap_addresses):
         """Connect to bootstrap peers and gather available peers."""
         deferreds = []
@@ -233,18 +256,15 @@ class P2PFactory(protocol.Factory):
         peer = peer_protocol.transport.getPeer()
         peer_address = PeerAddress(peer.type, peer.host, peer.port, peer.node_uuid)
         LOGGER.info(
-            "%sConnected to bootstrap peer %s:%d (UUID: %s)%s",
+            "%sConnected to bootstrap peer %s:%d%s",
             GREEN_COLOR,
             peer.host,
             peer.port,
-            peer.node_uuid,
             RESET_COLOR,
         )
         self.bootstrap_peers.append(peer_address)
-        # Wait for the UUID to be received in dataReceived
-        peer_protocol.transport.write(
-            json.dumps({"uuid": self.node_uuid}).encode("utf-8")
-        )
+        # Send local UUID to the bootstrap node
+        peer_protocol.send_handshake_init()
 
     def handle_peer_uuid(self, peer_protocol, peer_uuid):
         """Handle the received UUID from a peer."""

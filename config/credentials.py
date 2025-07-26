3d3a4877bd2a742e0c22692bbe2b8ce347dd8fd6
frstrtr <frstrtr@gmail.com>
2025-07-26 21:06:46 +0400

    [update] .venv.sample storage

--- config/credentials.py
+++ config/credentials.py
@@ -3,6 +3,11 @@ Handles bot credentials and configuration for the Telegram bot.
 """
 
 from os import getenv
+from dotenv import load_dotenv
+import logging
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 class Credentials:
@@ -11,20 +16,12 @@ class Credentials:
     def __init__(self):
         # Bot token can be obtained via https://t.me/BotFather
         self.bot_token = getenv("BOT_TOKEN")
-        self.target_audit_channel_id = -1002624042904  # Added attribute
+        self.target_audit_channel_id_str = getenv("TARGET_AUDIT_CHANNEL_ID")
 
-        if not self.bot_token:
-            self.bot_token = self.read_token_from_file()
-
-    def read_token_from_file(self):
-        """Read the bot token from token.txt"""
-        try:
-            with open("./config/token.txt", "r", encoding="utf-8") as file:
-                return file.read().strip()
-        except FileNotFoundError as exc:
+        if not self.bot_token or not self.target_audit_channel_id_str:
             raise RuntimeError(
-                "Bot token not found. Please set the BOT_TOKEN environment variable or create a token.txt file."
-            ) from exc
+                "Bot token or target audit channel ID not found. Please set them in the .env file."
+            )
 
     def get_bot_token(self):
         """Function to get the bot token"""
@@ -32,4 +29,4 @@ class Credentials:
 
     def get_target_audit_channel_id(self):
         """Function to get the target audit channel ID"""
-        return self.target_audit_channel_id
+        return self.target_audit_channel_id_str
